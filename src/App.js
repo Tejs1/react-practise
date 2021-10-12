5377c64e2c286f501976923430476cc81523e0e3
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-10-12 08:44:30 +0530

    WARM UP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import TodoList from "./TodoList";
+const alert = () => {
+  console.log("hi there");
+};
 function App() {
-  
-  return <>
-  <TodoList />
-  <input type="text" />
-  </>
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>create</button>
+      <button onClick={alert}>clear complete</button>
+      <div>Hello</div>
+    </>
+  );
 }
 
 export default App;
