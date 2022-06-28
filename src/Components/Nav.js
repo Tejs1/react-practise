d4b4eb233486f693dc11bdda712cdf144b131f52
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-06-29 00:07:29 +0530

    Active styliongs to nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,55 +5,39 @@ import { useTheme } from "../Context/theme-context";
 export function Nav() {
   const { theme, changeTheme } = useTheme();
   const { changeLang } = useLocal();
-  // const active = {
-  //   fontWeight: "bold",
-  //   color: "red",
-  // };
+  function getStyle() {
+    const active = {
+      fontWeight: "bold",
+      color: "green",
+    };
+    const inactive = {
+      fontWeight: "bold",
+      color: "red",
+    };
+    return ({ isActive }) => (isActive ? active : inactive);
+  }
 
   return (
     <nav>
       <h1 className="app-header">ECommerce </h1>
       <div>
         <button className={theme}>
-          <NavLink
-            to="/"
-            style={(isActive) => ({
-              color: isActive ? "white" : "blue",
-            })}
-          >
+          <NavLink to="/" style={getStyle()}>
             STORE
           </NavLink>
         </button>
         <button className={theme}>
-          <NavLink
-            to="cart"
-            style={(isActive) => ({
-              color: isActive ? "green" : "blue",
-            })}
-          >
-            {" "}
+          <NavLink to="cart" style={getStyle()}>
             CART
           </NavLink>
         </button>
         <button className={theme}>
-          <NavLink
-            to="whishlist"
-            style={(isActive) => ({
-              color: isActive ? "green" : "blue",
-            })}
-          >
-            {" "}
+          <NavLink to="whishlist" style={getStyle()}>
             Whishlist
           </NavLink>
         </button>
         <button className={theme}>
-          <NavLink
-            to="auth"
-            style={(isActive) => ({
-              color: isActive ? "green" : "blue",
-            })}
-          >
-            {" "}
+          <NavLink to="auth" style={getStyle()}>
             AUTH
           </NavLink>
         </button>
