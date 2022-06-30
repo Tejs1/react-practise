1f3ff4bd721371e1db769fee285da1138189d612
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-06-30 23:13:18 +0530

    routing programmatically

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Auth } from "./Components/Auth";
 import { ProductListing } from "./Components/ProductListing";
 import { Product } from "./Components/Product";
 import { useTheme } from "./Context/theme-context";
+import User from "./Components/User";
 function App() {
   const { theme } = useTheme();
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="cart" element={<Cart />} />
             <Route path="whishlist" element={<Wishlist />} />
             <Route path="auth" element={<Auth />} />
+            <Route path="user" element={<User />} />
             <Route path="product/:id" element={<Product />} />
             <Route
               path="*"
@@ -35,3 +37,7 @@ function App() {
 }
 
 export { App };
+//create login use page
+//add login page user pager to nav routes
+//create input and click handler in loginb page
+//if pass? nav : set err(true)
--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Auth() {
+  const [pass, setPass] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const loginHandler = () => {
+    if (pass === "1212") {
+      navigate("/user");
+    } else {
+      setPass("");
+      setError(true);
+    }
+  };
+
   return (
     <section>
-      <h1>Auth</h1>
+      <h1>ENTRY MANTRA</h1>
+      <input type="password" onChange={inputHandler} value={pass} />
+      <p>{pass}</p>
+      <button onClick={loginHandler}>Submit</button>
+      {error && <p>wrong pass</p>}
     </section>
   );
+
+  function inputHandler(e) {
+    setPass(e.target.value);
+    setError(false);
+  }
 }
